55f1555bcdcce1056f77ca916fcfe4a18b84e45f
deeksha puri <deekshapuri15@gmail.com>
2025-09-20 09:05:43 +0530

    Add files via upload

---
+++ Q9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() { 
+    int num ,den;
+    printf("enter num");
+    scanf("%d",&num);
+    printf("enter the den");
+    scanf(" %d", &den);
+    printf(" the fraction is: %d/%d ",num, den);
+    return 0;
+}
+
