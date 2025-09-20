9f0284739073f5af5cee8dfc7b5d9bec6bb6a512
deeksha puri <deekshapuri15@gmail.com>
2025-09-20 09:36:15 +0530

    Add files via upload

---
+++ Q12.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() { 
+    int r;
+    float area;
+    printf("enter the r",r);
+    scanf("%d", &r);
+    area = (22.0/7) *r*r;
+    printf("the area is : %.2f",area);
+    scanf(" %d %d",&r);
+
+    return 0;
+}
+
